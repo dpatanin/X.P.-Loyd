b28b5ee010dcc9806f5931e3bf31ade1d03bd5cc
dpatanin <da-patanin@t-online.de>
2022-12-30 15:51:41 +0100

    finish guide
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # X.P.-Loyd
 
 Currently this hosts an example project to get familiar with programming an RL AI in general.
+It utilizes a RL Deep-Q Learning Algorithm for predicting Stock Market trades.
 Here's the [guide].
 When installing the deps use the latest versions if the guide writes a specific versions, those are outdated.
-
-Currently the project is build up to [this point](<https://www.mlq.ai/deep-reinforcement-learning-for-trading-with-tensorflow-2-0/#:~:text=predict(actions%5B0%5D)-,Training%20the%20Model,-Now%20that%20we%27ve>).
+Be aware that the guide is not technically correct nor uses best practices, thus the differences in code.
 
 [guide]: https://www.mlq.ai/deep-reinforcement-learning-for-trading-with-tensorflow-2-0/
--- master.py
+++ master.py
@@ -4,23 +4,25 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import pandas_datareader as data_reader
+from pandas_datareader import data as pdr
 
 from tqdm import tqdm_notebook, tqdm
 from collections import deque
 
+import yfinance as yf
+yf.pdr_override()
 
 class AI_Trader():
 
     def __init__(self, state_size, action_space=3, model_name="AITrader"):
 
 
-        self.model = self.model_builer
-        self.state_size
+        self.state_size = state_size
         self.action_space = action_space
-        self.memory = deque(2000)
+        self.memory = deque(maxlen = 2000)
         self.inventory = []
         self.model_name = model_name
+        self.model = self.model_builder()
 
         self.gammsa = 0.95
         self.epsilon = 1.0
@@ -32,17 +34,116 @@ class AI_Trader():
 
         model = tf.keras.models.Sequential()
 
-        model.add(tf.layers.Dense(
+        model.add(tf.keras.layers.Dense(
             units=32, activation='relu', input_dim=self.state_size))
-        model.add(tf.layers.Dense(units=64, activation='relu'))
-        model.add(tf.layers.Dense(units=128, activation='relu'))
-        model.add(tf.layers.Dense(units=self.action_space, activation='linear'))
-        model.compile(loss='mse', optimizer=tf.keras.optimizer.Adam(lr=0.001))
+        model.add(tf.keras.layers.Dense(units=64, activation='relu'))
+        model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+        model.add(tf.keras.layers.Dense(
+            units=self.action_space, activation='linear'))
+        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=0.001))
 
         return model
 
     def trade(self, state):
-      if random.random() <= self.epsilon:
-          return random.randrange(self.action_space)
+        if random.random() <= self.epsilon:
+            return random.randrange(self.action_space)
 
-      actions = self.model.predict(actions[0])
+        actions = self.model.predict(actions[0])
+
+    def batch_train(self, batch_size):
+
+        batch = [
+            self.memory[i]
+            for i in range(len(self.memory) - batch_size + 1, len(self.memory))
+        ]
+        
+        for state, action, reward, next_state, done in batch:
+            reward = reward
+            if not done:
+                reward = reward + self.gamma * \
+                        np.amax(self.model.predict(next_state)[0])
+
+            target = self.model.predict(state)
+            target[0][action] = reward
+
+            self.model.fit(state, target, epochs=1, verbose=0)
+
+        if self.epsilon > self.epsilon_final:
+            self.epsilon *= self.epsilon_decay
+
+def sigmoid(x):
+        return (1 + math.exp(-x))
+
+def stocks_price_format(n):
+    return "- # {0:2f}".format(abs(n)) if n < 0 else "$ {0:2f}".format(abs(n))
+
+def state_creator(data, timestep, window_size):
+    starting_id = timestep - window_size + 1
+
+    windowed_data = (
+        data[starting_id : timestep + 1]
+        if starting_id >= 0
+        else starting_id * [data[0]] + list(data[: timestep + 1])
+    )
+    state = [
+        sigmoid(windowed_data[i + 1] - windowed_data[i])
+        for i in range(window_size - 1)
+    ]
+    return np.array([state])
+
+# Loading data
+data = pdr.get_data_yahoo("AAPL", start="2017-01-01", end="2017-04-30")
+
+window_size = 10
+episodes = 1000
+
+batch_size = 32
+data_samples = len(data) - 1
+
+trader = AI_Trader(window_size)
+trader.model.summary()
+
+
+for episode in range(1, episodes + 1):
+
+    print(f"Episode: {episode}/{episodes}")
+
+    state = state_creator(data, 0, window_size + 1)
+
+    total_profit = 0
+    trader.inventory = []
+
+    for t in tqdm(range(data_samples)):
+
+        action = trader.trade(state)
+
+        next_state = state_creator(data, t+1, window_size + 1)
+        reward = 0
+
+        if action == 1:  # Buying
+          trader.inventory.append(data[t])
+          print("AI Trader bought: ", stocks_price_format(data[t]))
+
+        elif action == 2 and len(trader.inventory) > 0:  # Selling
+          buy_price = trader.inventory.pop(0)
+
+          reward = max(data[t] - buy_price, 0)
+          total_profit += data[t] - buy_price
+          print("AI Trader sold: ", stocks_price_format(
+              data[t]), " Profit: " + stocks_price_format(data[t] - buy_price))
+
+        done = t == data_samples - 1
+        trader.memory.append((state, action, reward, next_state, done))
+
+        state = next_state
+
+        if done:
+            print("########################")
+            print(f"TOTAL PROFIT: {total_profit}")
+            print("########################")
+
+        if len(trader.memory) > batch_size:
+          trader.batch_train(batch_size)
+
+    if episode % 10 == 0:
+        trader.model.save(f"ai_trader_{episode}.h5")
