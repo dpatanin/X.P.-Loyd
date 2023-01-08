df02df6ab282a4251706272327b10859fd78081b
dpatanin <da-patanin@t-online.de>
2023-01-08 14:02:38 +0100

    update debug printer
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- debug_printer.py
+++ debug_printer.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 from tensorflow.python.client import device_lib
 
-#* Use this file to run debug print scripts 
+#* Use this file to run debug print scripts
+spacer = "##################################################################"
+
+print(spacer) 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print(spacer)
+print(spacer)
 print(device_lib.list_local_devices())
