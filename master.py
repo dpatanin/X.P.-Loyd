61c298340d1f9336dc40280fac4678014b955e85
dpatanin <da-patanin@t-online.de>
2023-01-28 15:52:52 +0100

    refactor code into reusable class
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- master.py
+++ master.py
@@ -4,38 +4,57 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-# load the data
-data = pd.read_csv("futures.csv")
 
-# create a new column with the difference of the close price
-data['diff'] = data['Close'] - data['Close'].shift(1)
+class FinancialModel:
+    def __init__(self, window_size=60):
+        self.window_size = window_size
+        self.scaler = MinMaxScaler()
+        self.model = LinearRegression()
 
-# drop missing values
-data.dropna(inplace=True)
+    def load_data(self, file_path):
+        self.data = pd.read_csv(file_path)
+        self.data["diff"] = self.data["Close"] - self.data["Close"].shift(1)
+        self.data.dropna(inplace=True)
+        self.data[
+            ["Open", "High", "Low", "Close", "Volume"]
+        ] = self.scaler.fit_transform(
+            self.data[["Open", "High", "Low", "Close", "Volume"]]
+        )
 
-# normalize the data
-scaler = MinMaxScaler()
-data[['Open', 'High', 'Low', 'Close', 'Volume']] = scaler.fit_transform(data[['Open', 'High', 'Low', 'Close', 'Volume']])
+    def create_windowed_dataset(self):
+        self.X = []
+        self.y = []
+        for i in range(self.window_size, len(self.data)):
+            self.X.append(
+                self.data[i - self.window_size : i][["Open", "High", "Low", "Volume"]]
+            )
+            self.y.append(self.data["Close"][i])
+        self.X = np.array(self.X)
+        self.y = np.array(self.y)
 
-# create the windowed dataset
-window_size = 60
-X = []
-y = []
-for i in range(window_size, len(data)):
-    X.append(data[i-window_size:i][['Open', 'High', 'Low', 'Volume']])
-    y.append(data['Close'][i])
+    def split_data(self, test_size=0.2):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.X, self.y, test_size=test_size
+        )
+        self.X_train = self.X_train.reshape(
+            self.X_train.shape[0], self.X_train.shape[1] * self.X_train.shape[2]
+        )
+        self.X_test = self.X_test.reshape(
+            self.X_test.shape[0], self.X_test.shape[1] * self.X_test.shape[2]
+        )
 
-# split into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(np.array(X), np.array(y), test_size=0.2)
+    def train_model(self):
+        self.model.fit(self.X_train, self.y_train)
 
-# reshape dim-3 array to dim-2
-X_train = X_train.reshape(X_train.shape[0], X_train.shape[1]*X_train.shape[2])
-X_test = X_test.reshape(X_test.shape[0], X_test.shape[1]*X_test.shape[2])
+    def evaluate_model(self):
+        score = self.model.score(self.X_test, self.y_test)
+        print("Model score: {:.2f}".format(score))
 
-# create and train the model
-model = LinearRegression()
-model.fit(X_train, y_train)
 
-# evaluate the model
-score = model.score(X_test, y_test)
-print("Model score: {:.2f}".format(score))
+# Usage example
+model = FinancialModel()
+model.load_data("futures.csv")
+model.create_windowed_dataset()
+model.split_data()
+model.train_model()
+model.evaluate_model()
