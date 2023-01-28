e9cf506351da5388647406e3182a27858260f2aa
dpatanin <da-patanin@t-online.de>
2023-01-28 16:32:53 +0100

    add model save & load methods
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- master.py
+++ master.py
@@ -3,7 +3,10 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 import os
+import pickle
+
 
 class FinancialModel:
     def __init__(self, window_size=60):
@@ -13,13 +16,15 @@ class FinancialModel:
         self.data = None
 
     def load_data(self, file_path: str):
-        assert os.path.exists(file_path), f'{file_path} does not exist.'
+        assert os.path.exists(file_path), f"{file_path} does not exist."
         self.data = pd.read_csv(file_path)
-        
-         # Check for the presence of specific columns
-        required_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
-        assert set(required_columns).issubset(self.data.columns), f'File {file_path} does not contain the required columns: {required_columns}'
-        
+
+        # Check for the presence of specific columns
+        required_columns = ["Open", "High", "Low", "Close", "Volume"]
+        assert set(required_columns).issubset(
+            self.data.columns
+        ), f"File {file_path} does not contain the required columns: {required_columns}"
+
         self.data["diff"] = self.data["Close"] - self.data["Close"].shift(1)
         self.data.dropna(inplace=True)
         self.data[
@@ -41,7 +46,7 @@ class FinancialModel:
 
     def split_data(self, test_size=0.2):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            self.X, self.y, test_size=test_size
+            self.X, self.y, test_size=test_size, random_state=42
         )
         self.X_train = self.X_train.reshape(
             self.X_train.shape[0], self.X_train.shape[1] * self.X_train.shape[2]
@@ -54,14 +59,28 @@ class FinancialModel:
         self.model.fit(self.X_train, self.y_train)
 
     def evaluate_model(self):
-        score = self.model.score(self.X_test, self.y_test)
-        print("Model score: {:.2f}".format(score))
+        y_pred = self.model.predict(self.X_test)
+        mse = mean_squared_error(y_pred, self.y_test)
+        r2 = r2_score(y_pred, self.y_test)
+        print("Model score: {:.2f}".format(r2))
+        print("Mean Squared Error: {:.2f}".format(mse))
+
+    def save_model(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self.model, f)
+
+    def load_model(self, filename):
+        with open(filename, "rb") as f:
+            self.model = pickle.load(f)
+
+    def fit_model(self):
+        self.create_windowed_dataset()
+        self.split_data()
+        self.train_model()
+        self.evaluate_model()
 
 
-# Usage example
 model = FinancialModel()
-model.load_data("futures.csv")
-model.create_windowed_dataset()
-model.split_data()
-model.train_model()
-model.evaluate_model()
+model.load_data("data/ES_futures_sample/ES_continuous_1min_sample.csv")
+model.fit_model()
+model.save_model("models/financial_model.pkl")
