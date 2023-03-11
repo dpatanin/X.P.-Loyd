62d721a8a582b7f80f3f5772f97266136741e178
dpatanin <da-patanin@t-online.de>
2023-03-11 17:01:56 +0100

    move hyperparameter to yaml
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- master.py
+++ master.py
@@ -3,63 +3,35 @@ from src.trader import FreeLaborTrader
 from src.state import State
 from src.action_space import ActionSpace
 import pandas as pd
+import yaml
+from yaml.loader import FullLoader
 
-
-# General
-tick_size = 0.25
-tick_value = 12.50
-init_balance = 10000.00
-data_dir = "data"
-model_dir = "models"
-model_name = "v1-prototype"
-
-# Action Space
-episodes = 100
-threshold = 0.2
-trade_limit = 500  # Limit to trade at once
-
-# Data structure
-headers = ["Open", "High", "Low", "Close", "Volume"]
-batch_size = 4
-sequence_length = 5
-
-# Agent params
-update_freq = 5
-memory_size = 2000
-gamma = 0.95
-epsilon = 1.0
-epsilon_final = 0.01
-epsilon_decay = 0.995
-
-# Learning & rewards
-terminal_reward_fac = 1.5
-intrinsic_reward_fac = 0.75
-hindsight_reward_fac = 0.5
-
+with open("config.yaml") as f:
+    config = yaml.load(f, Loader=FullLoader)
 
 action_space = ActionSpace(
-    threshold=threshold,
-    price_per_contract=tick_value,
-    limit=trade_limit,
-    intrinsic_fac=intrinsic_reward_fac,
+    threshold=config["action_space"]["threshold"],
+    price_per_contract=config["tick_value"],
+    limit=config["action_space"]["trade_limit"],
+    intrinsic_fac=config["reward_factors"]["intrinsic"],
 )
 dp = DataProcessor(
-    dir=data_dir,
-    sequence_length=sequence_length,
-    batch_size=batch_size,
-    headers=headers,
+    dir=config["data_directory"],
+    sequence_length=config["sequence_length"],
+    batch_size=config["batch_size"],
+    headers=config["data_headers"],
 )
 trader = FreeLaborTrader(
-    sequence_length=sequence_length,
-    batch_size=batch_size,
-    num_features=len(headers) + 3,
-    memory_size=memory_size,
-    update_freq=update_freq,
-    hindsight_reward_fac=hindsight_reward_fac,
-    gamma=gamma,
-    epsilon=epsilon,
-    epsilon_final=epsilon_final,
-    epsilon_decay=epsilon_decay,
+    sequence_length=config["sequence_length"],
+    batch_size=config["batch_size"],
+    num_features=len(config["data_headers"]) + 3,
+    memory_size=config["agent"]["memory_size"],
+    update_freq=config["agent"]["update_frequency"],
+    hindsight_reward_fac=config["reward_factors"]["hindsight"],
+    gamma=config["agent"]["gamma"],
+    epsilon=config["agent"]["epsilon"],
+    epsilon_final=config["agent"]["epsilon_final"],
+    epsilon_decay=config["agent"]["epsilon_decay"],
 )
 trader.model.summary()
 
@@ -73,18 +45,18 @@ def create_state(sequence: pd.DataFrame, state: "State" = None):
             contracts=state.contracts,
         )
         if state
-        else State(sequence, balance=init_balance)
+        else State(sequence, balance=config["initial_balance"])
     )
 
 
 for i in range(len(dp.batched_dir) - 1):
     batch = dp.load_batch(i)
 
-    for e in range(1, episodes + 1):
+    for e in range(1, config["episodes"] + 1):
         done = False
 
         # States to maintain continuity of actions
-        con_states: list["State"] = [None] * batch_size
+        con_states: list["State"] = [None] * config["batch_size"]
 
         for idx, sequences in enumerate(batch):
             if done:
@@ -111,7 +83,8 @@ for i in range(len(dp.batched_dir) - 1):
                 terminal_rewards: list[float] = [
                     -1000000000000000000
                     if ns.has_position() or ns.balance < 0
-                    else (r + ns.balance - init_balance) * terminal_reward_fac
+                    else (r + ns.balance - config["initial_balance"])
+                    * config["reward_factors"]["session_total"]
                     for r, ns in zip(rewards, next_states)
                 ]
 
@@ -119,7 +92,7 @@ for i in range(len(dp.batched_dir) - 1):
             for s, q, r, ns in zip(curr_states, q_values, rewards, next_states):
                 trader.memory.add((s, q, r, ns, done))
 
-            if len(trader.memory) > batch_size:
+            if len(trader.memory) > config["batch_size"]:
                 trader.batch_train()
 
         # Create hindsight experiences
@@ -127,6 +100,8 @@ for i in range(len(dp.batched_dir) - 1):
 
         # Save the model every 10 episodes
         if e % 10 == 0:
-            trader.model.save(f"{model_dir}/{model_name}_ep{e}.h5")
+            trader.model.save(
+                f"{config['model_directory']}/{config['model_name']}_ep{e}.h5"
+            )
 
-    trader.model.save(f"{model_dir}/{model_name}_terminal.h5")
+    trader.model.save(f"{config['model_directory']}/{config['model_name']}_terminal.h5")
