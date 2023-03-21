5cd23d11d4e1ae37352c8e53a2a4caaf0e1559af
dpatanin <da-patanin@t-online.de>
2023-03-21 16:56:04 +0100

    create progress bar
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- master.py
+++ master.py
@@ -2,6 +2,7 @@ from src.data_processor import DataProcessor
 from src.trader import FreeLaborTrader
 from src.state import State
 from src.action_space import ActionSpace
+from src.progress_bar import ProgressBar
 import pandas as pd
 import numpy as np
 import yaml
@@ -58,10 +59,11 @@ def calc_terminal_reward(reward: float, state: "State") -> float:
         ]["session_total"]
 
 
-def print_time(t: float, num_ep: int):
+def rem_time(t: float, num_ep: int):
     rem_time_sec = num_ep * (time.time() - t)
-    print(
-        f"remaining time: {round(rem_time_sec / 60)} min {round(rem_time_sec) % 60} sec")
+    return (
+        f"Remaining time: {round(rem_time_sec / 60)} min {round(rem_time_sec) % 60} sec"
+    )
 
 
 ########################### Training ###########################
@@ -69,24 +71,30 @@ def print_time(t: float, num_ep: int):
 dp.dir = config["training_data"]
 dp.batched_dir = dp.batch_dir()
 terminal_model = (
-        f"{config['model_directory']}/"
-        + f"{config['model_name']}_"
-        + f"{now}"
-        + "_terminal.h5"
+    f"{config['model_directory']}/"
+    + f"{config['model_name']}_"
+    + f"{now}"
+    + "_terminal.h5"
 )
-
+pbar = ProgressBar(
+    episodes=config["episodes"],
+    batches=len(dp.batched_dir),
+    sequences_per_batch=len(dp.load_batch(0)),
+    prefix="Training",
+    suffix="Remaining time: ???",
+    leave=True,
+)
 
 for e in range(1, config["episodes"] + 1):
     t1 = time.time()
-    for i in range(len(dp.batched_dir) - 1):
+    for i in range(len(dp.batched_dir)):
         batch = dp.load_batch(i)
         done = False
 
         # Initial states
         states = [
-                     State(data=empty_sequence(),
-                           balance=config["initial_balance"])
-                 ] * config["batch_size"]
+            State(data=empty_sequence(), balance=config["initial_balance"])
+        ] * config["batch_size"]
 
         for idx, sequences in enumerate(batch):
             for seq, state in zip(sequences, states):
@@ -94,13 +102,11 @@ for e in range(1, config["episodes"] + 1):
             snapshot = states.copy()  # States before action; For experiences
 
             q_values = trader.predict(states)
-            rewards = [action_space.take_action(q, s) for q, s in
-                       zip(q_values, states)]
+            rewards = [action_space.take_action(q, s) for q, s in zip(q_values, states)]
 
             done = idx == len(batch) - 1
             if done:
-                rewards = [calc_terminal_reward(r, s) for r, s in
-                           zip(rewards, states)]
+                rewards = [calc_terminal_reward(r, s) for r, s in zip(rewards, states)]
 
             for snap, reward, state in zip(snapshot, rewards, states.copy()):
                 trader.memory.add((snap, reward, state, done))
@@ -108,17 +114,20 @@ for e in range(1, config["episodes"] + 1):
             if len(trader.memory) > config["batch_size"]:
                 trader.batch_train()
 
+            pbar.update(e, i + 1, idx + 1)
+
         # Create hindsight experiences
         trader.memory.analyze_missed_opportunities(action_space)
 
-        # Save the model every 10 episodes
-        if e % 10 == 0:
-            trader.model.save(
-                f"{config['model_directory']}/{config['model_name']}_ep{e}_{now}.h5"
-            )
+    # Save the model every 10 episodes
+    if e % 10 == 0:
+        trader.model.save(
+            f"{config['model_directory']}/{config['model_name']}_ep{e}_{now}.h5"
+        )
+    pbar.suffix = rem_time(t1, config["episodes"] + 1 - e)
 
-    trader.model.save(terminal_model)
-    print_time(t1, config["episodes"] + 1 - e)
+trader.model.save(terminal_model)
+pbar.close()
 
 ###################### Validation | Test #######################
 
@@ -129,18 +138,32 @@ dp.step_size = 1
 trader.memory.clear()
 trader.load(terminal_model)
 
+pbar = ProgressBar(
+    episodes=1,
+    batches=len(dp.batched_dir),
+    sequences_per_batch=len(dp.load_batch(0)),
+    prefix="Validation",
+    suffix="Remaining time: ???",
+    leave=True,
+)
+
 for i in range(len(dp.batched_dir) - 1):
+    t1 = time.time()
     batch = dp.load_batch(i)
 
     # Initial states
-    states = [
-                 State(data=empty_sequence(), balance=config["initial_balance"])
-             ] * config["batch_size"]
+    states = [State(data=empty_sequence(), balance=config["initial_balance"])] * config[
+        "batch_size"
+    ]
 
-    for sequences in batch:
+    for idx, sequences in enumerate(batch):
         for seq, state in zip(sequences, states):
             state.data = seq
 
         q_values = trader.predict(states)
         for q, s in zip(q_values, states):
             action_space.take_action(q, s)
+
+        pbar.update(batch=i + 1, seq=idx)
+
+    pbar.suffix = rem_time(t1, len(dp.batched_dir) - i)
---
+++ src/progress_bar.py
@@ -0,0 +1,45 @@
+import time
+from tqdm import tqdm
+
+
+class ProgressBar(tqdm):
+    def __init__(
+        self,
+        episodes: int,
+        batches: int,
+        sequences_per_batch: int,
+        prefix: str = None,
+        suffix: str = None,
+        leave: bool = None,
+    ):
+        self.episodes = episodes
+        self.curr_ep = 1
+        self.batches = batches
+        self.curr_b = 1
+        self.sequences_per_batch = sequences_per_batch
+        self.curr_s = 1
+        self.prefix = prefix
+        self.suffix = suffix
+        self.total_iterations = episodes * batches * sequences_per_batch
+        super().__init__(
+            total=self.total_iterations,
+            leave=leave,
+            desc=self.gen_description(),
+        )
+
+    def gen_description(self): 
+        start = f'{self.prefix}|' if self.prefix else ''
+        end = f'|{self.suffix}' if self.suffix else ''
+        return f"{start}Episode {self.curr_ep}/{self.episodes}|Batch {self.curr_b}/{self.batches}|Sequence {self.curr_s}/{self.sequences_per_batch}{end}"
+
+    def update(self, ep: int = None, batch: int = None, seq: int = None):
+        diff_e = (ep - self.curr_ep) * self.batches * self.sequences_per_batch
+        diff_b = (batch - self.curr_b) * self.sequences_per_batch
+        diff_s = seq - self.curr_s
+        super().update(diff_e + diff_b + diff_s)
+
+        super().set_description
+        self.curr_ep = ep or self.curr_ep
+        self.curr_b = batch or self.curr_b
+        self.curr_s = seq or self.curr_s
+        super().set_description(self.gen_description())
