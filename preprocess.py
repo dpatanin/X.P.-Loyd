14b0af509d50c3874175c15033509e24e240be62
dpatanin <da-patanin@t-online.de>
2023-03-22 17:22:29 +0100

    normalize data
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@ from yaml.loader import FullLoader
 with open("config.yaml") as f:
     config = yaml.load(f, Loader=FullLoader)
 
-if config["training_set"] + config["validation_set"] + config["test_set"] != 0:
+if config["training_set"] + config["validation_set"] + config["test_set"] != 1:
     raise ValueError(
         "Data set ration doesn't add up to 1."
         + "If you want to reduce the total size used, set `total_set` accordingly."
@@ -45,7 +45,7 @@ valid_size = round(len(indices) * config["total_set"] * config["validation_set"]
 train_valid_indices = indices[:-test_size]
 random.shuffle(train_valid_indices)
 train_indices = train_valid_indices[: train_size - 1]
-valid_indices = train_valid_indices[train_indices : valid_size - 1]
+valid_indices = train_valid_indices[train_size : valid_size - 1]
 
 
 def generate(indices: list[tuple], names: list[str], path: str, label: str):
@@ -58,6 +58,13 @@ def generate(indices: list[tuple], names: list[str], path: str, label: str):
         progress = [(100 / timesteps) * x for x in range(1, timesteps + 1)]
         session.drop(["DateTime"], axis=1, inplace=True)
         session.insert(0, "Progress", progress)
+
+        # Normalizing
+        session["Open"] -= session["Open"].iloc[0]
+        session["High"] -= session["High"].iloc[0]
+        session["Low"] -= session["Low"].iloc[0]
+        # Keeping original close prizes for calculations
+        session["CloseNorm"] = session["Close"] - session["Close"].iloc[0]
         # -----------------------------
 
         session.to_csv(f"{path}/{name}.csv", sep=",", index=False)
