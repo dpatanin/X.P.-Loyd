d4a5f74dc9603b573b30f1b5d43c7b786a643b35
Daniel Patanin <44839597+dpatanin@users.noreply.github.com>
2023-03-28 14:36:26 +0200

    normalize data (#18)
    * normalize data
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * remove ellipsis
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * improve preprocessing
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * fix exclusiveness mistake
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * follow up
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    ---------
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@ from yaml.loader import FullLoader
 with open("config.yaml") as f:
     config = yaml.load(f, Loader=FullLoader)
 
-if config["training_set"] + config["validation_set"] + config["test_set"] != 0:
+if config["training_set"] + config["validation_set"] + config["test_set"] != 1:
     raise ValueError(
         "Data set ration doesn't add up to 1."
         + "If you want to reduce the total size used, set `total_set` accordingly."
@@ -19,19 +19,14 @@ pd.set_option("mode.chained_assignment", None)
 print("Reading data source...")
 data = pd.read_csv(config["source_data"])
 names = ["%05d" % x for x in range(100000)]
-start_indices = []
-end_indices = []
 
-for idx, row in tqdm(data.iterrows(), desc="Indexing...", leave=True):
-    if row[config["marker_column"]].endswith(config["session_begin_marker"]):
-        start_indices.append(idx)
-    if row[config["marker_column"]].endswith(config["session_end_marker"]):
-        end_indices.append(idx)
-
-if end_indices[0] < start_indices[0]:
-    end_indices.pop(0)
-if start_indices[-1] > end_indices[-1]:
-    start_indices.pop(-1)
+print("Indexing...")
+start_indices = data[
+    data[config["marker_column"]].str.contains(config["session_marker"])
+].index
+end_indices = (start_indices - 1).to_list()
+end_indices.pop(0)
+end_indices.append(len(data.index) - 1)
 indices = [*zip(start_indices, end_indices)]
 
 print("Splitting & shuffling...")
@@ -44,8 +39,8 @@ train_size = round(len(indices) * config["total_set"] * config["training_set"])
 valid_size = round(len(indices) * config["total_set"] * config["validation_set"])
 train_valid_indices = indices[:-test_size]
 random.shuffle(train_valid_indices)
-train_indices = train_valid_indices[: train_size - 1]
-valid_indices = train_valid_indices[train_indices : valid_size - 1]
+train_indices = train_valid_indices[:train_size]
+valid_indices = train_valid_indices[train_size : valid_size + train_size]
 
 
 def generate(indices: list[tuple], names: list[str], path: str, label: str):
@@ -58,6 +53,13 @@ def generate(indices: list[tuple], names: list[str], path: str, label: str):
         progress = [(100 / timesteps) * x for x in range(1, timesteps + 1)]
         session.drop(["DateTime"], axis=1, inplace=True)
         session.insert(0, "Progress", progress)
+
+        # Normalizing
+        session["Open"] -= session["Open"].iloc[0]
+        session["High"] -= session["High"].iloc[0]
+        session["Low"] -= session["Low"].iloc[0]
+        # Keeping original close prizes for calculations
+        session["CloseNorm"] = session["Close"] - session["Close"].iloc[0]
         # -----------------------------
 
         session.to_csv(f"{path}/{name}.csv", sep=",", index=False)
@@ -65,19 +67,19 @@ def generate(indices: list[tuple], names: list[str], path: str, label: str):
 
 generate(
     train_indices,
-    names[: train_size - 1],
+    names[:train_size],
     config["training_data"],
-    "Generating training set...",
+    "Generating training set",
 )
 generate(
     valid_indices,
-    names[train_size : valid_size - 1],
+    names[train_size : valid_size + train_size],
     config["validation_data"],
-    "Generating validation set...",
+    "Generating validation set",
 )
 generate(
     test_indices,
-    names[valid_size : test_size - 1],
+    names[valid_size + train_size : valid_size + train_size + test_size],
     config["test_data"],
-    "Generating test set...",
+    "Generating test set",
 )
