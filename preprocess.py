b4f971dc12d74128e6d807c25bcd4e1e95e9dd43
dpatanin <da-patanin@t-online.de>
2023-03-29 16:15:49 +0200

    rework preprocessing
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- preprocess.py
+++ preprocess.py
@@ -4,12 +4,18 @@ import random
 import yaml
 from yaml.loader import FullLoader
 
+total_set = 1  # How much of the total data to use
+training_set = 0.7  # Cross training-validation
+validation_set = 0.15
+test_set = 0.15  # Always latest data
+timesteps = 1320
+
 with open("config.yaml") as f:
     config = yaml.load(f, Loader=FullLoader)
 
-if config["training_set"] + config["validation_set"] + config["test_set"] != 1:
+if training_set + validation_set + test_set != 1:
     raise ValueError(
-        "Data set ration doesn't add up to 1."
+        "Data set ratio doesn't add up to 1."
         + "If you want to reduce the total size used, set `total_set` accordingly."
     )
 
@@ -17,26 +23,40 @@ if config["training_set"] + config["validation_set"] + config["test_set"] != 1:
 pd.set_option("mode.chained_assignment", None)
 
 print("Reading data source...")
-data = pd.read_csv(config["source_data"])
+data = pd.read_csv("data/source.csv")
 names = ["%05d" % x for x in range(100000)]
 
 print("Indexing...")
-start_indices = data[
-    data[config["marker_column"]].str.contains(config["session_marker"])
-].index
-end_indices = (start_indices - 1).to_list()
-end_indices.pop(0)
-end_indices.append(len(data.index) - 1)
-indices = [*zip(start_indices, end_indices)]
+start_indices = data[data["DateTime"].str.endswith("18:00:00")].index.to_list()
+end_indices = data[data["DateTime"].str.endswith("16:00:00")].index.to_list()
+
+indices = [(s, "s") for s in start_indices]
+indices.extend([(e, "e") for e in end_indices])
+indices.sort()
+
+# Remove incomplete indices
+anomalies = []
+marker = ""
+for i in indices:
+    if marker == "s" and i[1] == "s":
+        anomalies.append(indices[indices.index(i) - 1][0])
+    elif marker == "e" and i[1] == "e":
+        anomalies.append(i[0])
+    marker = i[1]
+
+indices = [i[0] for i in indices if i[0] not in anomalies]
+indices = [
+    (s, e) for s, e in zip(indices[::2], indices[1::2]) if e - s <= timesteps * 1.0625
+]
 
 print("Splitting & shuffling...")
 # Use always latest data for test
-test_size = round(len(indices) * config["total_set"] * config["test_set"])
+test_size = round(len(indices) * total_set * test_set)
 test_indices = indices[-test_size:-1]
 
 # Shuffle & split remaining data
-train_size = round(len(indices) * config["total_set"] * config["training_set"])
-valid_size = round(len(indices) * config["total_set"] * config["validation_set"])
+train_size = round(len(indices) * total_set * training_set)
+valid_size = round(len(indices) * total_set * validation_set)
 train_valid_indices = indices[:-test_size]
 random.shuffle(train_valid_indices)
 train_indices = train_valid_indices[:train_size]
@@ -49,9 +69,8 @@ def generate(indices: list[tuple], names: list[str], path: str, label: str):
         session = data.iloc[start : end + 1]
 
         # ------ Manipulate data ------
-        timesteps = end + 1 - start
-        progress = [(100 / timesteps) * x for x in range(1, timesteps + 1)]
         session.drop(["DateTime"], axis=1, inplace=True)
+        progress = [(100 / timesteps) * x for x in range(1, timesteps + 1)]
         session.insert(0, "Progress", progress)
 
         # Normalizing
