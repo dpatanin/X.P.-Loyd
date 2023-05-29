70d6cb6e13f1c4b12705f8da0fd8b382c2a993c9
dpatanin <da-patanin@t-online.de>
2023-05-29 20:42:16 +0200

    load model
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import tensorflow as tf
 import json
 
 PORT = 8080
+
+print("Loading AI...")
+model: tf.keras.Sequential = tf.keras.models.load_model("ai-training\models\prototype-V1_terminal_19_05_2023 11_23_45.h5")
+print("AI loaded")
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -10,8 +15,8 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
-        self.wfile.write(json.dumps({ "name":"John", "age":30, "city":"New York"}).encode('utf-8'))
+        self.wfile.write(json.dumps({ "config":model.get_config()}).encode('utf-8'))
 
 with HTTPServer(("", PORT), handler) as httpd:
-    print("serving at port", PORT)
+    print("Serving at port", PORT)
     httpd.serve_forever()
