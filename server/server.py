f44ea0bef46b4b3879494985c45fe3b677f71e97
dpatanin <da-patanin@t-online.de>
2023-06-16 20:03:47 +0200

    add logging & adjust request handling WIP
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- server/server.py
+++ server/server.py
@@ -1,24 +1,24 @@
 import requests
 from flask import Flask, request, jsonify
+import logging
 
 app = Flask(__name__)
+# Configure logging
+logging.basicConfig(filename='server.log', level=logging.DEBUG)
 
 # Endpoint for handling GET requests
-@app.route('/', methods=['GET'])
+@app.route('/predict', methods=['POST'])
 def handle_get_request():
-    # Read the content of the GET request
-    content = request.args.get('content')
+    content = request.json
+    logging.debug(f'Received content: {content}')
 
     # Make a request to the model
     model_response = requests.post('http://tensorflow-serve:8501/v1/models/prototype-V1:predict', json={'instances': [content]})
 
-    # Print the model response
-    print('Model Response:', model_response.json())
+    logging.debug(f'Model Response: {model_response.json()}')
 
-    # Extract the predicted result from the model response
     predicted_result = model_response.json()
 
-    # Send the predicted result as a response back to the client
     return jsonify({'result': predicted_result})
 
 if __name__ == '__main__':
