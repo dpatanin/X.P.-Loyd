82f085715b787659c46a9935cc8a503bedd68d59
dpatanin <da-patanin@t-online.de>
2023-06-16 15:57:08 +0200

    wip
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- server/server.py
+++ server/server.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import tensorflow as tf
+import keras as k
+
+# on thread 1
+session = tf.Session(graph=tf.Graph())
+with session.graph.as_default():
+    k.backend.set_session(session)
+    model = k.models.load_model("model/1/saved_model.pb")
+
+# on thread 2
+with session.graph.as_default():
+    k.backend.set_session(session)
+    model.predict(x, **kwargs)
