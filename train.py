9defaa44abf1b744cad35f6c14b9bb0643a6bcbd
dpatanin <da-patanin@t-online.de>
2023-06-17 19:01:05 +0200

    flat punishment for negative terminal balance
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- train.py
+++ train.py
@@ -59,7 +59,7 @@ def calc_terminal_reward(reward: float, state: "State") -> float:
     if state.has_position():
         reward += state.exit_position(config["tick_value"])
     if state.balance < 0:
-        return reward - 10000000000000000000000
+        return -10000000000000000000000000
     else:
         return (reward + state.balance - config["initial_balance"]) * config[
             "reward_factors"
