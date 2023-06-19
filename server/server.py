9e0f6e5a50297456a4680b5c1c2515063a63224f
Daniel Patanin <44839597+dpatanin@users.noreply.github.com>
2023-06-19 20:25:50 +0200

    Feature/local server (#23)
    * move requirements to root
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * set base server
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * change title
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * build simple get request solution
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * load model
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * initiate default django project for server
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * update readme
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * change timezone
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * remove database
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * revert
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * migrate
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * add default admin user
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * remove django app
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * add basic dockerfile for tensorflow serve
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * wip
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * adjust save location
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * readjust saving
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * create basic docker compose setup
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * base python server setup
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * add logging & adjust request handling WIP
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * preprocess request data
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * change to correct order
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * prepare for actions
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * fix data preprocessing
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * add documentation
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * remove validation block
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * simplify preprocessing to whole dataset
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * restructure project
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * adjust namings
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * introduce constants
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * Revert "introduce constants"
    
    This reverts commit 0aadfb2ab868cc5a9d0657291788e47c8deb8266.
    
    * Revert "adjust namings"
    
    This reverts commit 8815277f846bff12f4f1f9704fd47466a05f22c1.
    
    * Revert "restructure project"
    
    This reverts commit a893bc76aa6d10ea29c0fdf0d7bf5945f1e63cdb.
    
    * Revert "simplify preprocessing to whole dataset"
    
    This reverts commit 036723e26c7a10bd45516dda4906bde196fad067.
    
    * Revert "remove validation block"
    
    This reverts commit 15e1113d7780f0b5b0229d9423b521c53538a795.
    
    ---------
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>

---
+++ server/server.py
@@ -0,0 +1,55 @@
+import requests
+from flask import Flask, request, jsonify
+import logging
+import pandas as pd
+
+#! Ensure to keep the same order like in training
+HEADERS = [
+    "progress",
+    "open",
+    "high",
+    "low",
+    "close",
+    "volume",
+    "closeNorm",
+]
+STATE_VALS = [
+    "contracts",
+    "entryPrice",
+    "balance",
+]
+
+PPC = 12.50
+LIMIT = 75
+THRESHOLD = 0.2
+
+app = Flask(__name__)
+# Configure logging
+logging.basicConfig(filename="server.log", level=logging.DEBUG)
+
+
+# Endpoint for handling GET requests
+@app.route("/predict", methods=["POST"])
+def handle_get_request():
+    logging.debug(f"Request received: {request}")
+
+    # Read data in order
+    data = pd.DataFrame({header: request.json[header] for header in HEADERS})
+    for sv in STATE_VALS:
+        data[sv] = [request.json[sv]] * len(request.json[HEADERS[0]])
+    logging.debug(f"Data read: {str(data)}")
+
+    # Make a request to the model
+    model_response = requests.post(
+        "http://tensorflow-serve:8501/v1/models/prototype-V1:predict",
+        json={"instances": [data.to_numpy().tolist()]},
+    )
+    logging.debug(f"Model Response: {model_response.json()}")
+
+    predicted_result: float = model_response.json()
+
+    return jsonify({"result": predicted_result})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
