4efa27cf83c5d3d66f15abf57a10e88a72eb374d
dpatanin <da-patanin@t-online.de>
2023-08-05 10:26:05 +0200

    finish first rework setup
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import shutil
 from datetime import datetime
 
-import tensorflow as tf
+import keras
+import pandas as pd
 import yaml
 from yaml.loader import FullLoader
 
@@ -13,6 +14,8 @@ with open("config.yaml") as f:
     config = yaml.load(f, Loader=FullLoader)
 
 now = datetime.now().strftime("%d_%m_%Y %H_%M_%S")
+model_dir = f"{config['model_directory']}/{config['description']}_{now}/"
+
 dp = DataProcessor(config["data_src"])
 wg = WindowGenerator(
     input_width=config["sequence_length"],
@@ -22,32 +25,74 @@ wg = WindowGenerator(
     batch_size=config["batch_size"],
 )
 
-model_dir = f"{config['model_directory']}/{config['description']}_{now}/"
-tb_callback = tf.keras.callbacks.TensorBoard(log_dir=config["log_dir"], update_freq=100)
-early_stopping = tf.keras.callbacks.EarlyStopping(
+tb_callback = keras.callbacks.TensorBoard(log_dir=config["log_dir"], update_freq=100)
+early_stopping = keras.callbacks.EarlyStopping(
     monitor="val_loss", patience=2, mode="min"
 )
-optimizer = tf.keras.optimizers.Adamax()
-loss = tf.keras.losses.MeanSquaredError()
-metrics = [tf.keras.metrics.MeanAbsoluteError()]
+optimizer = keras.optimizers.Adamax()
+loss = keras.losses.MeanSquaredError()
+metrics = [keras.metrics.MeanAbsoluteError()]
 
-# Autoregressive time series forecast
-tb_callback.log_dir = (
-    f"{config['log_dir']}/{config['description']}_{now}/autoregressive_{now}"
-)
-ar_model = Autoregressive(64, config["prediction_length"], dp.num_features)
-ar_model.compile(
-    loss=loss,
-    optimizer=optimizer,
-    metrics=metrics,
-)
-ar_model.fit(
-    wg.make_dataset(dp.train_df),
-    epochs=config["episodes"],
-    validation_data=wg.make_dataset(dp.val_df),
-    callbacks=[tb_callback, early_stopping],
-)
-ar_model.evaluate(wg.make_dataset(dp.test_df), verbose=0, callbacks=[tb_callback])
 
-ar_model.save(f"{model_dir}auto_regressive/", save_format="tf")
-shutil.copy2("config.yaml", f"{model_dir}auto_regressive/")
+def compile_and_fit(
+    model,
+    name: str,
+    train: pd.DataFrame = None,
+    val: pd.DataFrame = None,
+    test: pd.DataFrame = None,
+):
+    tb_callback.log_dir = (
+        f"{config['log_dir']}/{config['description']}_{now}/{name}_{now}"
+    )
+
+    model.compile(
+        loss=loss,
+        optimizer=optimizer,
+        metrics=metrics,
+    )
+    model.fit(
+        wg.make_dataset(train or dp.train_df),
+        epochs=config["episodes"],
+        validation_data=wg.make_dataset(val or dp.val_df),
+        callbacks=[tb_callback, early_stopping],
+    )
+    model.evaluate(
+        wg.make_dataset(test or dp.test_df), verbose=0, callbacks=[tb_callback]
+    )
+
+    model.save(f"{model_dir}{name}/", save_format="tf")
+    shutil.copy2("config.yaml", f"{model_dir}{name}/")
+
+
+def single_shot():
+    return keras.Sequential(
+        [
+            keras.layers.LSTM(64, return_sequences=False),
+            keras.layers.Dense(
+                config["prediction_length"],
+                kernel_initializer=keras.initializers.zeros(),
+            ),
+        ]
+    )
+
+
+lstm_close = single_shot()
+compile_and_fit(model=lstm_close, name="lstm_close")
+
+wg.compute_indices(shift=config["prediction_length"] + 1, label_columns=["open"])
+lstm_open = single_shot()
+compile_and_fit(model=lstm_open, name="lstm_open")
+
+wg.compute_indices(
+    shift=config["prediction_length"],
+    data_columns=dp.train_df[["close"]].columns,
+    label_columns=["close"],
+)
+ar_model = Autoregressive(32, config["prediction_length"])
+compile_and_fit(
+    model=ar_model,
+    name="autoregressive",
+    train=dp.train_df[["close"]],
+    val=dp.val_df[["close"]],
+    test=dp.test_df[["close"]],
+)
