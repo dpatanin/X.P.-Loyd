0af2063dcfe8f7e946e146e3e5493ecb09dcddbb
dpatanin <da-patanin@t-online.de>
2023-08-05 15:20:43 +0200

    fix stuff
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- train.py
+++ train.py
@@ -25,14 +25,6 @@ wg = WindowGenerator(
     batch_size=config["batch_size"],
 )
 
-tb_callback = keras.callbacks.TensorBoard(log_dir=config["log_dir"], update_freq=100)
-early_stopping = keras.callbacks.EarlyStopping(
-    monitor="val_loss", patience=2, mode="min"
-)
-optimizer = keras.optimizers.Adamax()
-loss = keras.losses.MeanSquaredError()
-metrics = [keras.metrics.MeanAbsoluteError()]
-
 
 def compile_and_fit(
     model,
@@ -41,23 +33,28 @@ def compile_and_fit(
     val: pd.DataFrame = None,
     test: pd.DataFrame = None,
 ):
-    tb_callback.log_dir = (
-        f"{config['log_dir']}/{config['description']}_{now}/{name}_{now}"
+    tb_callback = keras.callbacks.TensorBoard(
+        log_dir=f"{config['log_dir']}/{config['description']}_{now}/{name}_{now}",
+        update_freq=100,
     )
+    early_stopping = keras.callbacks.EarlyStopping(
+        monitor="val_loss", patience=2, mode="min"
+    )
+    optimizer = keras.optimizers.Adamax()
+    loss = keras.losses.MeanSquaredError()
+    metrics = [keras.metrics.MeanAbsoluteError()]
 
-    model.compile(
-        loss=loss,
-        optimizer=optimizer,
-        metrics=metrics,
-    )
+    model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
     model.fit(
-        wg.make_dataset(train or dp.train_df),
+        wg.make_dataset(train if train is not None else dp.train_df),
         epochs=config["episodes"],
-        validation_data=wg.make_dataset(val or dp.val_df),
+        validation_data=wg.make_dataset(val if val is not None else dp.val_df),
         callbacks=[tb_callback, early_stopping],
     )
     model.evaluate(
-        wg.make_dataset(test or dp.test_df), verbose=0, callbacks=[tb_callback]
+        wg.make_dataset(test if test is not None else dp.test_df),
+        verbose=0,
+        callbacks=[tb_callback],
     )
 
     model.save(f"{model_dir}{name}/", save_format="tf")
