eb4944cf4968b31f8577537f1a7b60f102cf9d93
dpatanin <da-patanin@t-online.de>
2023-08-07 20:12:19 +0200

    fix data processing
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- lib/data_processor.py
+++ lib/data_processor.py
@@ -21,32 +21,25 @@ class DataProcessor:
             df = pd.read_csv(file_path)
             os.remove(file_path)
 
+        self.num_features = df.shape[1]
+
         # Transform periodic frequencies
         date_time = pd.to_datetime(df.pop("dateTime"), format="%Y-%m-%d %H:%M:%S")
         timestamp_s = date_time.map(pd.Timestamp.timestamp)
+        day = 24 * 60 * 60
 
-        df["day_sin"] = np.sin(timestamp_s * (2 * np.pi / 60))
-        df["day_cos"] = np.cos(timestamp_s * (2 * np.pi / 60))
+        df["day_sin"] = np.sin(timestamp_s * (2 * np.pi / day))
+        df["day_cos"] = np.cos(timestamp_s * (2 * np.pi / day))
 
-        volumes = df.pop("volume")
-        df["volume_sin"] = np.sin(volumes * (2 * np.pi / 60))
-        df["volume_cos"] = np.cos(volumes * (2 * np.pi / 60))
+        # Standardize price data as returns
+        for col in ["high", "low", "open", "close"]:
+            df[col] = df[col].pct_change()
 
         # Split data
         n = len(df)
-        train_df = df[: int(n * 0.7)]
-        val_df = df[int(n * 0.7) : int(n * 0.9)]
-        test_df = df[int(n * 0.9) :]
-
-        self.num_features = df.shape[1]
-
-        # Standardization; EMA used during training for further normalization
-        train_mean = train_df.mean()
-        train_std = train_df.std()
-
-        self.train_df = (train_df - train_mean) / train_std
-        self.val_df = (val_df - train_mean) / train_std
-        self.test_df = (test_df - train_mean) / train_std
+        self.train_df = df[: int(n * 0.7)]
+        self.val_df = df[int(n * 0.7) : int(n * 0.9)]
+        self.test_df = df[int(n * 0.9) :]
 
     def is_local(self, url):
         url_parsed = urlparse(url)
