8d7c20ed9a8b9428e49afd3b26dc7df6021e2c92
Daniel Patanin <44839597+dpatanin@users.noreply.github.com>
2023-08-10 13:03:27 +0200

    Feature/ema (#43)
    * remove training & validation data
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * First step of rework: autoregressive DNN
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * save model
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * rework to single-shot complex layer DNN
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * implement autoregressive model
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * fix name
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * refactor
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * spelling
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * drop open price
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * clean up
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * cleanup file after download
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * rework to use one output fixed
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * rework for better reuse
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * finish first rework setup
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * fix stuff
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * train first models
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * add todo
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * redo window generator
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * add gru sentiment model
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * transform volumes to frequency
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * follow up fix
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * improve prints
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * reinstall requirements
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * redo requirements
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * make some data analysis
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * fix data processing
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * remove data analysis
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * improve price standardization &  use ema
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * remove config
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * use nadam optimizer
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * fix data columns
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * train models
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    ---------
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- lib/data_processor.py
+++ lib/data_processor.py
@@ -13,7 +13,7 @@ class DataProcessor:
     Splits into train (70%), validation (20%) & test (10%) datasets.
     """
 
-    def __init__(self, src: str) -> None:
+    def __init__(self, src: str, ema_period: int) -> None:
         if self.is_local(src):
             df = pd.read_csv(src)
         else:
@@ -21,32 +21,33 @@ class DataProcessor:
             df = pd.read_csv(file_path)
             os.remove(file_path)
 
-        # Transform periodic frequencies
+        print("\nProcessing data...")
+        self.num_features = df.shape[1]
+
+        # Transform dateTime into periodic frequencies
         date_time = pd.to_datetime(df.pop("dateTime"), format="%Y-%m-%d %H:%M:%S")
         timestamp_s = date_time.map(pd.Timestamp.timestamp)
+        day = 24 * 60 * 60
 
-        df["day_sin"] = np.sin(timestamp_s * (2 * np.pi / 60))
-        df["day_cos"] = np.cos(timestamp_s * (2 * np.pi / 60))
+        df["day_sin"] = np.sin(timestamp_s * (2 * np.pi / day))
+        df["day_cos"] = np.cos(timestamp_s * (2 * np.pi / day))
 
-        volumes = df.pop("volume")
-        df["volume_sin"] = np.sin(volumes * (2 * np.pi / 60))
-        df["volume_cos"] = np.cos(volumes * (2 * np.pi / 60))
+        # Preprocess price data
+        for col in ["high", "low"]:
+            df[col] = (df[col] - df["close"]).abs()
+
+        for col in ["open", "close"]:
+            series = df.pop(col)
+            df[f"{col}_pct"] = series.pct_change()
+            df[f"{col}_ema"] = series.ewm(span=ema_period, adjust=False).mean()
+
+        df.fillna(0, inplace=True)
 
         # Split data
         n = len(df)
-        train_df = df[: int(n * 0.7)]
-        val_df = df[int(n * 0.7) : int(n * 0.9)]
-        test_df = df[int(n * 0.9) :]
-
-        self.num_features = df.shape[1]
-
-        # Standardization; EMA used during training for further normalization
-        train_mean = train_df.mean()
-        train_std = train_df.std()
-
-        self.train_df = (train_df - train_mean) / train_std
-        self.val_df = (val_df - train_mean) / train_std
-        self.test_df = (test_df - train_mean) / train_std
+        self.train_df = df[: int(n * 0.7)]
+        self.val_df = df[int(n * 0.7) : int(n * 0.9)]
+        self.test_df = df[int(n * 0.9) :]
 
     def is_local(self, url):
         url_parsed = urlparse(url)
--- train.py
+++ train.py
@@ -1,49 +1,46 @@
-import shutil
 from datetime import datetime
 
 import keras
-import yaml
-from yaml.loader import FullLoader
 
 from lib.autoregressive import Autoregressive
 from lib.data_processor import DataProcessor
 from lib.window_generator import WindowGenerator
 
-with open("config.yaml") as f:
-    config = yaml.load(f, Loader=FullLoader)
+SRC_DATA = "https://onedrive.live.com/download?resid=2ba9b2044e887de1%21290022&authkey=!ADgq6YFliQNylSM"
+SENTIMENT_DATA = "https://onedrive.live.com/download?resid=2ba9b2044e887de1%21293628&authkey=!ANbFvs1RrC9WQ3c"
+
+DESC = "EMA-Implementation"
+EPOCHS = 10
+SEQ_LENGTH = 30
+PRED_LENGTH = 15
+BATCH_SIZE = 512
 
 now = datetime.now().strftime("%d_%m_%Y %H_%M_%S")
-model_dir = f"{config['model_directory']}/{config['description']}_{now}/"
-dp = DataProcessor(config["src_data"])
+dp = DataProcessor(src=SRC_DATA, ema_period=20)
 
 
 def compile_and_fit(model, name: str, train, val, test):
     tb_callback = keras.callbacks.TensorBoard(
-        log_dir=f"{config['log_dir']}/{config['description']}_{now}/{name}_{now}",
+        log_dir=f"logs/{DESC}__{now}/{name}",
         update_freq=100,
     )
     early_stopping = keras.callbacks.EarlyStopping(
         monitor="val_loss", patience=2, mode="min"
     )
-    optimizer = keras.optimizers.Adamax()
+    optimizer = keras.optimizers.Nadam()
     loss = keras.losses.MeanSquaredError()
     metrics = [keras.metrics.MeanAbsoluteError()]
 
     model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
     model.fit(
         train,
-        epochs=config["episodes"],
+        epochs=EPOCHS,
         validation_data=val,
         callbacks=[tb_callback, early_stopping],
     )
-    model.evaluate(
-        test,
-        verbose=0,
-        callbacks=[tb_callback],
-    )
+    model.evaluate(test, verbose=0, callbacks=[tb_callback])
 
-    model.save(f"{model_dir}{name}/", save_format="tf")
-    shutil.copy2("config.yaml", f"{model_dir}{name}/")
+    model.save(f"models/{DESC}__{now}/{name}/", save_format="tf")
 
 
 def single_shot():
@@ -51,86 +48,86 @@ def single_shot():
         [
             keras.layers.LSTM(64, return_sequences=False),
             keras.layers.Dense(
-                config["prediction_length"],
-                kernel_initializer=keras.initializers.zeros(),
+                PRED_LENGTH, kernel_initializer=keras.initializers.zeros()
             ),
         ]
     )
 
 
 print("\n--------------------------- Close LSTM ---------------------------")
+columns = ["day_sin", "day_cos", "high", "low", "close_pct", "close_ema", "volume"]
 wg_close = WindowGenerator(
-    input_width=config["sequence_length"],
-    label_width=config["prediction_length"],
-    data_columns=dp.train_df.columns,
-    label_columns=["close"],
-    batch_size=config["batch_size"],
+    input_width=SEQ_LENGTH,
+    label_width=PRED_LENGTH,
+    data_columns=columns,
+    label_columns=["close_pct"],
+    batch_size=BATCH_SIZE,
 )
 lstm_close = single_shot()
 compile_and_fit(
     model=lstm_close,
     name="lstm_close",
-    train=wg_close.make_dataset(dp.train_df),
-    val=wg_close.make_dataset(dp.val_df),
-    test=wg_close.make_dataset(dp.test_df),
+    train=wg_close.make_dataset(dp.train_df[columns]),
+    val=wg_close.make_dataset(dp.val_df[columns]),
+    test=wg_close.make_dataset(dp.test_df[columns]),
 )
 
 print("\n--------------------------- Open LSTM ---------------------------")
+columns = ["day_sin", "day_cos", "high", "low", "open_pct", "open_ema", "volume"]
 wg_open = WindowGenerator(
-    input_width=config["sequence_length"],
-    label_width=config["prediction_length"],
-    data_columns=dp.train_df.columns,
-    batch_size=config["batch_size"],
-    shift=config["prediction_length"] + 1,
-    label_columns=["open"],
+    input_width=SEQ_LENGTH,
+    label_width=PRED_LENGTH,
+    data_columns=columns,
+    batch_size=BATCH_SIZE,
+    shift=PRED_LENGTH + 1,
+    label_columns=["open_pct"],
 )
 lstm_open = single_shot()
 compile_and_fit(
     model=lstm_open,
     name="lstm_open",
-    train=wg_open.make_dataset(dp.train_df),
-    val=wg_open.make_dataset(dp.val_df),
-    test=wg_open.make_dataset(dp.test_df),
+    train=wg_open.make_dataset(dp.train_df[columns]),
+    val=wg_open.make_dataset(dp.val_df[columns]),
+    test=wg_open.make_dataset(dp.test_df[columns]),
 )
 
 print("\n--------------------------- Autoregressive ---------------------------")
+columns = ["open_ema"]
 wg_ar = WindowGenerator(
-    input_width=config["sequence_length"],
-    label_width=config["prediction_length"],
-    data_columns=dp.train_df[["close"]].columns,
-    batch_size=config["batch_size"],
+    input_width=SEQ_LENGTH,
+    label_width=PRED_LENGTH,
+    data_columns=columns,
+    batch_size=BATCH_SIZE,
 )
-ar_model = Autoregressive(32, config["prediction_length"])
+ar_model = Autoregressive(32, PRED_LENGTH, len(columns))
 compile_and_fit(
     model=ar_model,
     name="autoregressive",
-    train=wg_ar.make_dataset(dp.train_df[["close"]]),
-    val=wg_ar.make_dataset(dp.val_df[["close"]]),
-    test=wg_ar.make_dataset(dp.test_df[["close"]]),
+    train=wg_ar.make_dataset(dp.train_df[columns]),
+    val=wg_ar.make_dataset(dp.val_df[columns]),
+    test=wg_ar.make_dataset(dp.test_df[columns]),
 )
 
 print("\n--------------------------- GRU Sentiment ---------------------------")
-dp_sentiment = DataProcessor(config["sentiment_data"])
+columns = ["close_ema", "sentiment"]
+dp_sentiment = DataProcessor(src=SENTIMENT_DATA, ema_period=20)
 wg_gru = WindowGenerator(
-    input_width=config["sequence_length"],
-    label_width=config["prediction_length"],
-    data_columns=dp_sentiment.train_df[["close", "sentiment"]].columns,
-    batch_size=config["batch_size"],
-    label_columns=["close"],
+    input_width=SEQ_LENGTH,
+    label_width=PRED_LENGTH,
+    data_columns=columns,
+    batch_size=BATCH_SIZE,
+    label_columns=["close_ema"],
 )
 gru_model = keras.Sequential(
     [
         keras.layers.GRU(32, return_sequences=False),
-        keras.layers.Dense(
-            config["prediction_length"],
-            kernel_initializer=keras.initializers.zeros(),
-        ),
+        keras.layers.Dense(PRED_LENGTH, kernel_initializer=keras.initializers.zeros()),
     ]
 )
 compile_and_fit(
     model=gru_model,
     name="gru_sentiment",
-    train=wg_gru.make_dataset(dp_sentiment.train_df[["close", "sentiment"]]),
-    val=wg_gru.make_dataset(dp_sentiment.val_df[["close", "sentiment"]]),
-    test=wg_gru.make_dataset(dp_sentiment.test_df[["close", "sentiment"]]),
+    train=wg_gru.make_dataset(dp_sentiment.train_df[columns]),
+    val=wg_gru.make_dataset(dp_sentiment.val_df[columns]),
+    test=wg_gru.make_dataset(dp_sentiment.test_df[columns]),
 )
