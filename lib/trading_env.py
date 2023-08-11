be1ed6a92062ba21fc0517ef2392e63435e1232b
dpatanin <da-patanin@t-online.de>
2023-08-11 20:40:31 +0200

    advance trading env
    Signed-off-by: dpatanin <da-patanin@t-online.de>

--- lib/trading_env.py
+++ lib/trading_env.py
@@ -1,80 +1,218 @@
+from enum import Enum
+
 import gym
+import keras
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from gym import spaces
+
+from lib.ensemble import Ensemble
 
 
 class TradingEnvironment(gym.Env):
+    metadata = {"render.modes": ["human"]}
+
     def __init__(
         self,
-        lstm_forecast,
-        ar_forecast,
-        gru_forecast,
-        close_prices,
+        df: pd.DataFrame,
+        ensemble: Ensemble,
         balance=1000,
         tick_ratio=12.5 / 0.25,
+        fees_per_contract=0,
+        trade_volume=1,
     ):
         super(TradingEnvironment, self).__init__()
 
-        self.observation = self.init_observation = {
-            "lstm_forecast": lstm_forecast,
-            "ar_forecast": ar_forecast,
-            "gru_forecast": gru_forecast,
-            "close_prices": close_prices,
-            "position": 0,  # Initialize position to neutral (no position); 1 = Long; 2 = Short
-            "balance": balance,
-        }
-        self.entry_price = 0
-        self.tick_ratio = tick_ratio
+        self.df = df
+        self.ensemble = ensemble
+        self.window_size = ensemble.max_window_size
+        self.prices = df[["high", "low", "open", "close"]]
 
+        self.tick_ratio = tick_ratio
+        self.fees_per_contract = fees_per_contract
+        self.trade_volume = trade_volume  # TODO: Dynamic volumes?
+
+        # Actions & Position: 0 = No position; 1 = Long; 2 = Short
         self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Dict(
             {
                 "lstm_forecast": spaces.Box(
                     low=-np.inf,
                     high=np.inf,
-                    shape=lstm_forecast.shape,
+                    shape=(ensemble.lstm_window,),
                     dtype=np.float32,
                 ),
                 "ar_forecast": spaces.Box(
-                    low=-np.inf, high=np.inf, shape=ar_forecast.shape, dtype=np.float32
+                    low=-np.inf,
+                    high=np.inf,
+                    shape=(ensemble.ar_window,),
+                    dtype=np.float32,
                 ),
                 "gru_forecast": spaces.Box(
-                    low=-np.inf, high=np.inf, shape=gru_forecast.shape, dtype=np.float32
-                ),
-                "close_prices": spaces.Box(
-                    low=-np.inf, high=np.inf, shape=close_prices.shape, dtype=np.float32
+                    low=-np.inf,
+                    high=np.inf,
+                    shape=(ensemble.gru_window),
+                    dtype=np.float32,
                 ),
                 "position": spaces.Discrete(3),
+                "balance": spaces.Box(
+                    low=-np.inf, high=np.inf, shape=(1,), dtype=np.float32
+                ),
             }
         )
 
+        # episode
+        self._balance = self._init_balance = balance
+        self._start_tick = self.window_size
+        self._end_tick = len(self.df) - 1
+        self._done = None
+        self._current_tick = None
+        self._entry_price = 0
+        self._last_trade_tick = None
+        self._position = None
+        self._position_history = None
+        self._total_reward = None
+        self._total_profit = None
+        self._first_rendering = None
+        self.history = None
+
     def reset(self):
-        return self.init_observation
+        self._done = False
+        self._balance = self._init_balance
+        self._current_tick = self._start_tick
+        self._entry_price = 0
+        self._last_trade_tick = self._current_tick - 1
+        self._position = 0
+        self._position_history = (self.window_size * [None]) + [self._position]
+        self._total_reward = 0.0
+        self._total_profit = 1.0  # unit
+        self._first_rendering = True
+        self.history = {}
+        return self._get_observation()
 
     def step(self, action):
-        previous_position = self.observation["position"]
-        self.observation["position"] = action
-        next_observation = self.observation.copy()
+        self._current_tick += 1
 
-        reward = self.calculate_reward(
-            previous_position, action, self.observation["close_prices"][-1]
-        )
-        next_observation["balance"] += reward
-        done = next_observation["balance"] <= 0
-
-        return next_observation, reward, done
-
-    def calculate_reward(
-        self, previous_position, current_position, current_close_price
-    ):
-        if current_position != previous_position:
-            if previous_position == 1:  # Long position
-                reward = (current_close_price - self.entry_price) * self.tick_ratio
-            elif previous_position == 2:  # Short position
-                reward = (self.entry_price - current_close_price) * self.tick_ratio
-            else:  # No position
-                reward = 0
+        price_diff = 0
+        if action != self._position:
+            current_close_price = self.df["close"].iloc[self._current_tick]
+            if self._position == 1:
+                price_diff = current_close_price - self.entry_price
+            elif self._position == 2:
+                price_diff = self.entry_price - current_close_price
             self.entry_price = current_close_price
-        else:
-            reward = 0
-        return reward
+            self._last_trade_tick = self._current_tick
+
+        reward = self._calculate_reward(price_diff)
+        self._total_reward += reward
+        self._update_profit(price_diff)
+
+        self._done = self._balance <= 0 or self._current_tick == self._end_tick
+
+        self._position = action
+        self._position_history.append(self._position)
+        observation = self._get_observation()
+        info = dict(
+            total_reward=self._total_reward,
+            total_profit=self._total_profit,
+            position=self._position.value,
+        )
+        self._update_history(info)
+
+        return observation, reward, self._done, info
+
+    def _get_observation(self):
+        next_window = self.df[
+            (self._current_tick - self.window_size + 1) : self._current_tick + 1
+        ]
+        forecast = self.ensemble.forecast(next_window)
+
+        return {
+            "lstm_forecast": forecast["lstm"],
+            "ar_forecast": forecast["ar"],
+            "gru_forecast": forecast["gru"],
+            "position": self._position,
+            "balance": self._balance,
+        }
+
+    def _update_history(self, info):
+        if not self.history:
+            self.history = {key: [] for key in info.keys()}
+
+        for key, value in info.items():
+            self.history[key].append(value)
+
+    def render(self, mode="human"):
+        def _plot_position(position, tick):
+            match position:
+                case 0:
+                    color = "blue"
+                case 1:
+                    color = "green"
+                case 2:
+                    color = "red"
+                case _:
+                    color = None
+            if color:
+                plt.scatter(tick, self.prices[tick], color=color)
+
+        if self._first_rendering:
+            self._first_rendering = False
+            plt.cla()
+            plt.plot(self.prices)
+            start_position = self._position_history[self._start_tick]
+            _plot_position(start_position, self._start_tick)
+
+        _plot_position(self._position, self._current_tick)
+
+        plt.suptitle(
+            "Total Reward: %.6f" % self._total_reward
+            + " ~ "
+            + "Total Profit: %.6f" % self._total_profit
+        )
+
+        plt.pause(0.01)
+
+    def render_all(self, mode="human"):
+        window_ticks = np.arange(len(self._position_history))
+        plt.plot(self.prices)
+
+        short_ticks = []
+        long_ticks = []
+        for i, tick in enumerate(window_ticks):
+            if self._position_history[i] == 2:
+                short_ticks.append(tick)
+            elif self._position_history[i] == 1:
+                long_ticks.append(tick)
+
+        plt.plot(short_ticks, self.prices[short_ticks], "ro")
+        plt.plot(long_ticks, self.prices[long_ticks], "go")
+
+        plt.suptitle(
+            "Total Reward: %.6f" % self._total_reward
+            + " ~ "
+            + "Total Profit: %.6f" % self._total_profit
+        )
+
+    def close(self):
+        plt.close()
+
+    def save_rendering(self, filepath):
+        plt.savefig(filepath)
+
+    def pause_rendering(self):
+        plt.show()
+
+    def _calculate_reward(self, diff_price):
+        # TODO: Reward function?
+        return diff_price
+
+    def _update_profit(self, reward):
+        profit = self.trade_volume * (reward * self.tick_ratio - self.fees_per_contract)
+        self._balance += profit
+        self._total_profit += profit
+
+    def max_possible_profit(self):  # trade fees are ignored
+        # TODO: separate profits & fees -> track both for eval
+        raise NotImplementedError
--- train.py
+++ train.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
 import keras
+from ray.rllib.algorithms.r2d2 import r2d2
+from ray.tune.registry import register_env
 
 from lib.autoregressive import Autoregressive
 from lib.data_processor import DataProcessor
+from lib.ensemble import Ensemble
+from lib.trading_env import TradingEnvironment
 from lib.window_generator import WindowGenerator
 
 SRC_DATA = "https://onedrive.live.com/download?resid=2ba9b2044e887de1%21290022&authkey=!ADgq6YFliQNylSM"
@@ -55,11 +59,19 @@ def single_shot():
 
 
 print("\n--------------------------- Close LSTM ---------------------------")
-columns = ["day_sin", "day_cos", "high", "low", "close_pct", "close_ema", "volume"]
+lstm_close_columns = [
+    "day_sin",
+    "day_cos",
+    "high_diff",
+    "low_diff",
+    "close_pct",
+    "close_ema",
+    "volume",
+]
 wg_close = WindowGenerator(
     input_width=SEQ_LENGTH,
     label_width=PRED_LENGTH,
-    data_columns=columns,
+    data_columns=lstm_close_columns,
     label_columns=["close_pct"],
     batch_size=BATCH_SIZE,
 )
@@ -67,17 +79,25 @@ lstm_close = single_shot()
 compile_and_fit(
     model=lstm_close,
     name="lstm_close",
-    train=wg_close.make_dataset(dp.train_df[columns]),
-    val=wg_close.make_dataset(dp.val_df[columns]),
-    test=wg_close.make_dataset(dp.test_df[columns]),
+    train=wg_close.make_dataset(dp.train_df[lstm_close_columns]),
+    val=wg_close.make_dataset(dp.val_df[lstm_close_columns]),
+    test=wg_close.make_dataset(dp.test_df[lstm_close_columns]),
 )
 
 print("\n--------------------------- Open LSTM ---------------------------")
-columns = ["day_sin", "day_cos", "high", "low", "open_pct", "open_ema", "volume"]
+lstm_open_columns = [
+    "day_sin",
+    "day_cos",
+    "high_diff",
+    "low_diff",
+    "open_pct",
+    "open_ema",
+    "volume",
+]
 wg_open = WindowGenerator(
     input_width=SEQ_LENGTH,
     label_width=PRED_LENGTH,
-    data_columns=columns,
+    data_columns=lstm_open_columns,
     batch_size=BATCH_SIZE,
     shift=PRED_LENGTH + 1,
     label_columns=["open_pct"],
@@ -86,35 +106,35 @@ lstm_open = single_shot()
 compile_and_fit(
     model=lstm_open,
     name="lstm_open",
-    train=wg_open.make_dataset(dp.train_df[columns]),
-    val=wg_open.make_dataset(dp.val_df[columns]),
-    test=wg_open.make_dataset(dp.test_df[columns]),
+    train=wg_open.make_dataset(dp.train_df[lstm_open_columns]),
+    val=wg_open.make_dataset(dp.val_df[lstm_open_columns]),
+    test=wg_open.make_dataset(dp.test_df[lstm_open_columns]),
 )
 
 print("\n--------------------------- Autoregressive ---------------------------")
-columns = ["close_ema"]
+ar_columns = ["close_ema"]
 wg_ar = WindowGenerator(
     input_width=SEQ_LENGTH,
     label_width=PRED_LENGTH,
-    data_columns=columns,
+    data_columns=ar_columns,
     batch_size=BATCH_SIZE,
 )
-ar_model = Autoregressive(32, PRED_LENGTH, len(columns))
+ar_model = Autoregressive(32, PRED_LENGTH, len(ar_columns))
 compile_and_fit(
     model=ar_model,
     name="autoregressive",
-    train=wg_ar.make_dataset(dp.train_df[columns]),
-    val=wg_ar.make_dataset(dp.val_df[columns]),
-    test=wg_ar.make_dataset(dp.test_df[columns]),
+    train=wg_ar.make_dataset(dp.train_df[ar_columns]),
+    val=wg_ar.make_dataset(dp.val_df[ar_columns]),
+    test=wg_ar.make_dataset(dp.test_df[ar_columns]),
 )
 
 print("\n--------------------------- GRU Sentiment ---------------------------")
-columns = ["close_ema", "sentiment"]
+gru_columns = ["close_ema", "sentiment"]
 dp_sentiment = DataProcessor(src=SENTIMENT_DATA, ema_period=20)
 wg_gru = WindowGenerator(
     input_width=SEQ_LENGTH,
     label_width=PRED_LENGTH,
-    data_columns=columns,
+    data_columns=gru_columns,
     batch_size=BATCH_SIZE,
     label_columns=["close_ema"],
 )
@@ -127,7 +147,53 @@ gru_model = keras.Sequential(
 compile_and_fit(
     model=gru_model,
     name="gru_sentiment",
-    train=wg_gru.make_dataset(dp_sentiment.train_df[columns]),
-    val=wg_gru.make_dataset(dp_sentiment.val_df[columns]),
-    test=wg_gru.make_dataset(dp_sentiment.test_df[columns]),
+    train=wg_gru.make_dataset(dp_sentiment.train_df[gru_columns]),
+    val=wg_gru.make_dataset(dp_sentiment.val_df[gru_columns]),
+    test=wg_gru.make_dataset(dp_sentiment.test_df[gru_columns]),
 )
+
+print("\n--------------------------- R2D2 Trader ---------------------------")
+
+ensemble = Ensemble(
+    lstm_models_and_columns=[
+        (lstm_close, lstm_close_columns),
+        (lstm_open, lstm_open_columns),
+    ],
+    ar_model=ar_model,
+    ar_columns=ar_columns,
+    gru_model=gru_model,
+    gru_columns=gru_columns,
+    window=SEQ_LENGTH,
+)
+
+
+def env_creator(env_config):
+    return TradingEnvironment(dp_sentiment.train_df, ensemble)
+
+
+register_env("trading_env", env_creator)
+
+r2d2.R2D2(env="trading_env")
+
+# config = (
+#     R2D2Config()
+#     .environment(TradingEnvironment)
+#     .framework("tf")
+#     .training(
+#         model={
+#             "fcnet_hiddens": [64],
+#             "fcnet_activation": "linear",
+#             "use_lstm": True,
+#             "lstm_cell_size": 64,
+#             "max_seq_len": SEQ_LENGTH,
+#         }
+#     )
+#     .exploration(explore=True, exploration_config={"epsilon_timesteps": 50000})
+# )
+
+# algo = config.build()
+
+# for _ in range(5):
+#     print(algo.train())  # 3. train it,
+
+# algo.evaluate()  # 4. and evaluate it.
