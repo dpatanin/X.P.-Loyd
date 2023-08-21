b5141af2d1020ee284d5eed2833a48e2d7d5df0b
Daniel Patanin <44839597+dpatanin@users.noreply.github.com>
2023-08-21 14:47:11 +0200

    Feature/dqn (#44)
    * fix column
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * create gym trading environment
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * delete archived files
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * don't drop columns
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * install gym & plot
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * create ensembler
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * advance trading env
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * clean up
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * some fixes WIP
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * setup working ray cluster for DNNs
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * WIP
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * WIP
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * revert from ray
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * run training
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * Implement deep rl zoo's r2d2 algo WIP
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * WIP
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * attempt implementing tf_agents WIP
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * implement py wrapper
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * show progress of data processor
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * add progress visualization
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * remove r2d2 shenanigans
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * clean up imports
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * fix training
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * improve loggings
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * final fixes
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    * fix history output & train
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>
    
    ---------
    
    Signed-off-by: dpatanin <da-patanin@t-online.de>

---
+++ lib/trading_env.py
@@ -0,0 +1,284 @@
+import json
+from typing import Optional, Text
+
+import gymnasium as gym
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from gymnasium import spaces
+from tf_agents.environments import PyEnvironment
+from tf_agents.specs import BoundedArraySpec
+from tf_agents.trajectories import time_step as ts
+from tf_agents.typing import types
+
+
+class TradingEnvironment(gym.Env):
+    metadata = {"render.modes": ["human"]}
+
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        window_size: int,
+        features: list[str],
+        trade_limit=10,
+        balance=10000.00,
+        tick_ratio=12.5 / 0.25,
+        fees_per_contract=0.00,
+    ):
+        super(TradingEnvironment, self).__init__()
+
+        self.df = df
+        self.window_size = window_size
+        self.features = features
+        self.trade_limit = trade_limit
+        self.prices = df[["high", "low", "open", "close"]]
+        self.tick_ratio = tick_ratio
+        self.fees_per_contract = fees_per_contract
+
+        self.action_space = spaces.Box(low=-trade_limit, high=trade_limit, shape=(1,))
+
+        obs_shape = (2 + window_size * len(features),)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=obs_shape)
+
+        # episode
+        self._init_balance = balance
+        self._start_tick = self.window_size
+        self._end_tick = len(self.df)
+        self._episode_history = []
+
+        self.reset()
+
+    def reset(self):
+        self._done = False
+        self._truncated = False
+        self._balance = self._init_balance
+        self._current_tick = self._start_tick
+        self._entry_price = 0
+        self._total_profit = 0
+        self._total_fees = 0
+        self._position = 0
+        self._position_history = (self.window_size * [None]) + [self._position]
+        self._first_rendering = True
+        self.history = {}
+        self._update_observation()
+
+        return self.observation
+
+    def step(self, action: np.ndarray):
+        self._current_tick += 1
+        self._done = self._current_tick == self._end_tick
+        trade_volume = action[0]
+
+        profit = fees = 0.00
+        if trade_volume != self._position:
+            current_close_price = self.df["close"].iloc[self._current_tick]
+            price_diff = current_close_price - self._entry_price
+            self._entry_price = current_close_price
+
+            profit, fees = self._update_balance(price_diff, self._position)
+            self._truncated = self._balance <= 0
+
+        self._position = trade_volume
+        self._position_history.append(self._position)
+
+        self._update_observation()
+        info = self._get_info(profit, fees)
+        self._update_history(info)
+        if self._done or self._truncated:
+            self._episode_history.append(self.history)
+
+        return (
+            self.observation,
+            np.array(profit, dtype=np.float32),
+            self._done,
+            self._truncated,
+            info,
+        )
+
+    def _update_observation(self):
+        next_window = self.df[
+            (self._current_tick - self.window_size + 1) : self._current_tick + 1
+        ]
+
+        # Ensure order of inputs/outputs
+        self.observation = np.concatenate(
+            [
+                [self._position],
+                [self._balance],
+                *[next_window[feature].values for feature in self.features],
+            ],
+            dtype=self.observation_space.dtype,
+        )
+
+    def _update_history(self, info):
+        if not self.history:
+            self.history = {key: [] for key in info.keys()}
+
+        for key, value in info.items():
+            self.history[key].append(value)
+
+    def _update_balance(self, price_diff: float, trade_volume: int):
+        fees = abs(trade_volume) * self.fees_per_contract
+        self._total_fees += fees
+
+        profit = trade_volume * price_diff * self.tick_ratio
+        self._total_profit += profit
+
+        self._balance += profit - fees
+        return (profit, fees)
+
+    def _get_info(self, profit, fees):
+        return {
+            "profit": profit,
+            "total_profit": self._total_profit,
+            "fees": fees,
+            "total_fees": self._total_fees,
+            "balance": self._balance,
+            "position": self._position,
+        }
+
+    def render(self, mode="human"):
+        def _plot_position(position, tick):
+            if position == 0:
+                color = "blue"
+            elif position > 0:
+                color = "green"
+            elif position < 0:
+                color = "red"
+            else:
+                color = None
+            if color:
+                plt.scatter(tick, self.prices[tick], color=color)
+
+        if self._first_rendering:
+            self._first_rendering = False
+            plt.cla()
+            plt.plot(self.prices)
+            start_position = self._position_history[self._start_tick]
+            _plot_position(start_position, self._start_tick)
+
+        _plot_position(self._position, self._current_tick)
+        plt.pause(0.01)
+
+    def render_all(self, mode="human"):
+        window_ticks = np.arange(len(self._position_history))
+        plt.plot(self.prices)
+
+        short_ticks = []
+        long_ticks = []
+        for i, tick in enumerate(window_ticks):
+            if self._position_history[i] < 0:
+                short_ticks.append(tick)
+            elif self._position_history[i] > 0:
+                long_ticks.append(tick)
+
+        plt.plot(short_ticks, self.prices[short_ticks], "ro")
+        plt.plot(long_ticks, self.prices[long_ticks], "go")
+
+    def close(self):
+        plt.close()
+
+    def save_rendering(self, filepath: str):
+        plt.savefig(filepath)
+
+    def pause_rendering(self):
+        plt.show()
+
+    def observation_spec(self):
+        return self._get_space_spec(self.observation_space, "observation")
+
+    def action_spec(self):
+        return self._get_space_spec(self.action_space, "action")
+
+    def time_step_spec(self):
+        return ts.time_step_spec(self.observation_spec())
+
+    def _get_space_spec(self, space: gym.Space, name: str):
+        return BoundedArraySpec(
+            shape=space.shape,
+            dtype=space.dtype,
+            minimum=space.low,
+            maximum=space.high,
+            name=name,
+        )
+
+
+class PyTradingEnvWrapper(PyEnvironment):
+    def __init__(self, env: TradingEnvironment):
+        super().__init__()
+
+        self._env = env
+        self._latest_info = {}
+        self._reset()
+
+    def observation_spec(self) -> types.NestedArraySpec:
+        return self._env.observation_spec()
+
+    def action_spec(self) -> types.NestedArraySpec:
+        return self._env.action_spec()
+
+    def time_step_spec(self) -> ts.TimeStep:
+        return self._env.time_step_spec()
+
+    def render(self, mode: Text = "rgb_array") -> Optional[types.NestedArray]:
+        self._env.render()
+
+    def get_info(self) -> types.NestedArray:
+        def dict_to_nested_arrays(dictionary):
+            if isinstance(dictionary, dict):
+                nested_array = []
+                for key, value in dictionary.items():
+                    nested_value = dict_to_nested_arrays(value)
+                    nested_array.append((key, nested_value))
+                return nested_array
+            else:
+                return dictionary
+
+        return dict_to_nested_arrays(self._latest_info)
+
+    def _step(self, action: types.NestedArray) -> ts.TimeStep:
+        if self._current_time_step.is_last():
+            return self._reset()
+        observation, reward, done, truncated, info = self._env.step(action)
+
+        self._latest_info = info
+        self._current_time_step = ts.TimeStep(
+            step_type=ts.StepType.LAST if done or truncated else ts.StepType.MID,
+            reward=reward,
+            discount=self._discount,
+            observation=observation,
+        )
+
+        return self._current_time_step
+
+    def _reset(self) -> ts.TimeStep:
+        observation = self._env.reset()
+        self._discount = np.array(1.0, dtype=np.float32)  # Currently not in use
+        self._latest_info = {}
+        self._current_time_step = ts.TimeStep(
+            step_type=ts.StepType.FIRST,
+            reward=np.array(0.0, dtype=np.float32),
+            discount=self._discount,
+            observation=observation,
+        )
+
+        return self.current_time_step()
+
+    def save_episode_history(self, file_name: str):
+        def convert_to_json_serializable(obj):
+            if isinstance(obj, np.generic):
+                return obj.item()
+            elif isinstance(obj, (int, float, str)):
+                return obj
+            else:
+                raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+
+        with open(f"{file_name}.json", "w") as json_file:
+            if len(self._env._episode_history) > 0:
+                for ep in self._env._episode_history:
+                    for key, value_list in ep.items():
+                        ep[key] = [
+                            convert_to_json_serializable(value) for value in value_list
+                        ]
+
+            json.dump(self._env._episode_history, json_file)
---
+++ train_dqn.py
@@ -0,0 +1,285 @@
+import os
+import warnings
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "4"  # or any {0:5}
+warnings.simplefilter("ignore")
+import logging
+import tempfile
+from datetime import datetime
+
+import pandas as pd
+import reverb  # type: ignore
+import tensorflow as tf
+from tf_agents.agents.cql import cql_sac_agent
+from tf_agents.agents.ddpg import critic_rnn_network
+from tf_agents.agents.sac import tanh_normal_projection_network
+from tf_agents.metrics import py_metrics
+from tf_agents.networks import actor_distribution_rnn_network
+from tf_agents.policies import py_tf_eager_policy, random_py_policy
+from tf_agents.replay_buffers import reverb_replay_buffer, reverb_utils
+from tf_agents.train import actor, learner, triggers
+from tf_agents.train.utils import spec_utils, strategy_utils, train_utils
+from tqdm import tqdm
+
+from lib.data_processor import DataProcessor
+from lib.trading_env import PyTradingEnvWrapper, TradingEnvironment
+
+tempdir = tempfile.gettempdir()
+
+now = datetime.now().strftime("%d_%m_%Y %H_%M_%S")
+features = [
+    "day_sin",
+    "day_cos",
+    "high_diff",
+    "low_diff",
+    "open_pct",
+    "open_ema",
+    "close_pct",
+    "close_ema",
+    "volume",
+]
+
+FULL_DATA = "https://onedrive.live.com/download?resid=2ba9b2044e887de1%21290022&authkey=!ADgq6YFliQNylSM"  # No sentiment but ~15 years
+SENTIMENT_DATA = "https://onedrive.live.com/download?resid=2ba9b2044e887de1%21293628&authkey=!ANbFvs1RrC9WQ3c"  # With sentiment but ~5 years
+SEQ_LENGTH = 30
+BATCH_SIZE = 512
+TIME_STEPS = 100000
+
+dp = DataProcessor(FULL_DATA, 5)
+pb = tqdm(range(18), desc="Create environments")
+
+
+def update_pb(desc: str = None):
+    pb.update()
+    if desc:
+        pb.set_description(desc)
+
+
+def env_creator(df: pd.DataFrame):
+    return PyTradingEnvWrapper(
+        TradingEnvironment(
+            df=df,
+            window_size=SEQ_LENGTH,
+            features=features,
+            balance=10000.00,
+            fees_per_contract=0.25,
+            trade_limit=50,
+        )
+    )
+
+
+collect_env = env_creator(dp.train_df)
+eval_env = env_creator(dp.val_df)
+
+update_pb("Get specs")
+strategy = strategy_utils.get_strategy(tpu=False, use_gpu=True)
+
+observation_spec, action_spec, time_step_spec = spec_utils.get_tensor_specs(collect_env)
+
+with strategy.scope():
+    update_pb("Create critic net")
+    critic_net = critic_rnn_network.CriticRnnNetwork(
+        (observation_spec, action_spec),
+        observation_fc_layer_params=None,
+        action_fc_layer_params=None,
+        lstm_size=[64],
+        joint_fc_layer_params=(256, 256),
+        kernel_initializer="glorot_uniform",
+        last_kernel_initializer="glorot_uniform",
+    )
+
+    update_pb("Create actor net")
+    actor_net = actor_distribution_rnn_network.ActorDistributionRnnNetwork(
+        observation_spec,
+        action_spec,
+        lstm_size=[64],
+        continuous_projection_net=(
+            tanh_normal_projection_network.TanhNormalProjectionNetwork
+        ),
+    )
+
+    train_step = train_utils.create_train_step()
+
+    update_pb("Initialize cql-sac agent")
+    tf_agent = cql_sac_agent.CqlSacAgent(
+        time_step_spec,
+        action_spec,
+        actor_network=actor_net,
+        critic_network=critic_net,
+        actor_optimizer=tf.keras.optimizers.Adam(learning_rate=3e-4),
+        critic_optimizer=tf.keras.optimizers.Adam(learning_rate=3e-4),
+        alpha_optimizer=tf.keras.optimizers.Adam(learning_rate=3e-4),
+        cql_alpha=0.2,
+        num_cql_samples=5,
+        target_update_tau=0.005,
+        target_update_period=1,
+        td_errors_loss_fn=tf.math.squared_difference,
+        gamma=0.99,
+        gradient_clipping=1,
+        reward_scale_factor=1.0,
+        train_step_counter=train_step,
+        include_critic_entropy_term=False,
+        use_lagrange_cql_alpha=True,
+    )
+
+    tf_agent.initialize()
+
+update_pb("Create reverb replay")
+rate_limiter = reverb.rate_limiters.SampleToInsertRatio(
+    samples_per_insert=3.0, min_size_to_sample=3, error_buffer=3.0
+)
+
+table_name = "uniform_table"
+table = reverb.Table(
+    table_name,
+    max_size=10000,
+    sampler=reverb.selectors.Uniform(),
+    remover=reverb.selectors.Fifo(),
+    rate_limiter=reverb.rate_limiters.MinSize(1),
+)
+
+reverb_server = reverb.Server([table])
+
+reverb_replay = reverb_replay_buffer.ReverbReplayBuffer(
+    tf_agent.collect_data_spec,
+    sequence_length=2,
+    table_name="uniform_table",
+    local_server=reverb_server,
+)
+
+update_pb("Prefetch experiences")
+dataset = reverb_replay.as_dataset(sample_batch_size=BATCH_SIZE, num_steps=2).prefetch(
+    50
+)
+experience_dataset_fn = lambda: dataset
+
+update_pb("Create policies")
+tf_eval_policy = tf_agent.policy
+eval_policy = py_tf_eager_policy.PyTFEagerPolicy(tf_eval_policy, use_tf_function=True)
+update_pb()
+
+tf_collect_policy = tf_agent.collect_policy
+collect_policy = py_tf_eager_policy.PyTFEagerPolicy(
+    tf_collect_policy, use_tf_function=True
+)
+update_pb()
+
+random_policy = random_py_policy.RandomPyPolicy(
+    collect_env.time_step_spec(), collect_env.action_spec()
+)
+update_pb()
+
+rb_observer = reverb_utils.ReverbAddTrajectoryObserver(
+    reverb_replay.py_client, table_name, sequence_length=2, stride_length=1
+)
+
+update_pb("Run initial actor")
+initial_collect_actor = actor.Actor(
+    collect_env,
+    random_policy,
+    train_step,
+    steps_per_run=10000,
+    observers=[rb_observer],
+)
+initial_collect_actor.run()
+
+update_pb("Create collect actor")
+env_step_metric = py_metrics.EnvironmentSteps()
+collect_actor = actor.Actor(
+    collect_env,
+    collect_policy,
+    train_step,
+    steps_per_run=1,
+    metrics=actor.collect_metrics(10),
+    summary_dir=os.path.join(tempdir, learner.TRAIN_DIR),
+    observers=[rb_observer, env_step_metric],
+)
+
+update_pb("Create eval actor")
+eval_actor = actor.Actor(
+    eval_env,
+    eval_policy,
+    train_step,
+    episodes_per_run=20,
+    metrics=actor.eval_metrics(20),
+    summary_dir=os.path.join(tempdir, "eval"),
+)
+
+update_pb("Create triggers")
+saved_model_dir = os.path.join(tempdir, learner.POLICY_SAVED_MODEL_DIR)
+
+# Triggers to save the agent's policy checkpoints.
+learning_triggers = [
+    triggers.PolicySavedModelTrigger(
+        saved_model_dir, tf_agent, train_step, interval=5000
+    ),
+    triggers.StepPerSecondLogTrigger(train_step, interval=1000),
+]
+
+update_pb("Create learner")
+agent_learner = learner.Learner(
+    tempdir,
+    train_step,
+    tf_agent,
+    experience_dataset_fn,
+    triggers=learning_triggers,
+    strategy=strategy,
+)
+
+
+update_pb("Run eval actor")
+
+
+def get_eval_metrics():
+    eval_actor.run()
+    return {metric.name: metric.result() for metric in eval_actor.metrics}
+
+
+metrics = get_eval_metrics()
+
+
+def log_eval_metrics(step, metrics):
+    eval_results = (", ").join(
+        "{} = {:.6f}".format(name, result) for name, result in metrics.items()
+    )
+    print("step = {0}: {1}".format(step, eval_results))
+
+
+log_eval_metrics(0, metrics)
+
+# Reset the train step
+tf_agent.train_step_counter.assign(0)
+
+update_pb("Evaluate agent's policy")
+avg_return = get_eval_metrics()["AverageReturn"]
+returns = [avg_return]
+update_pb("Training prepared!")
+pb.close()
+
+with tqdm(range(TIME_STEPS), desc="Training") as pbar:
+    for _ in range(TIME_STEPS):
+        # Training.
+        collect_actor.run()
+        loss_info = agent_learner.run(iterations=1)
+
+        # Evaluating.
+        step = agent_learner.train_step_numpy
+
+        if step % 10000 == 0:
+            pbar.set_description("Evaluating")
+            metrics = get_eval_metrics()
+            log_eval_metrics(step, metrics)
+            returns.append(metrics["AverageReturn"])
+
+        pbar.set_description(f"Training | Loss: {loss_info.loss.numpy()}")
+
+        pbar.update()
+
+    try:
+        collect_env.save_episode_history(f"logs/episode-history__{now}")
+    except TypeError as error:
+        logging.error(error)
+
+tf.saved_model.save(tf_agent.policy, "/tmp/models/saved_model")
+rb_observer.close()
+reverb_server.stop()
