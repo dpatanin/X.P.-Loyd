9212fda73afe91bc91fe6812928303ce47a0aa5f
DennisNovak <d.novak1104@gmail.com>
2023-08-26 15:21:36 +0200

    improved efficiency, added Conditional operations

--- lib/SMACrossOver.py
+++ lib/SMACrossOver.py
@@ -3,42 +3,35 @@ import pandas as pd
 class SMACrossOver:
     def __init__(self, data_frame):
         self.data_frame = data_frame
-        self.long_triggered = False
-        self.short_triggered = False
+        self.data_frame['position'] = None
+        self.short_condition = False
+        self.long_condition = True
 
     def SMA(self, data, period):
-        return None if len(data) < period else sum(data[-period:]) / period
+        return data.rolling(window=period).mean()
 
     def CrossAbove(self, x, y):
-        if not self.long_triggered and x > y:
-            self.long_triggered = True
-            self.short_triggered = False
-            return True
-        return False
+        return x > y
 
     def CrossBelow(self, x, y):
-        if not self.short_triggered and x < y:
-            self.short_triggered = True
-            self.long_triggered = False
-            return True
-        return False
+        return x < y
 
-    def EnterShort(self, index):
-        self.data_frame.at[index, 'position'] = -1
+    def EnterShort(self):
+        self.data_frame['position'] = self.data_frame['position'].mask(self.short_condition, 2)
 
-    def EnterLong(self, index):
-        self.data_frame.at[index, 'position'] = 1
+    def EnterLong(self):
+        self.data_frame['position'] = self.data_frame['position'].mask(self.long_condition, 1)
 
 
     def analyze(self, periodFast, periodSlow):
-        self.data_frame['smaFast'] = self.data_frame['close'].rolling(window=periodFast).mean()
-        self.data_frame['smaSlow'] = self.data_frame['close'].rolling(window=periodSlow).mean()
+        self.data_frame['smaFast'] = self.SMA(self.data_frame['close'], periodFast)
+        self.data_frame['smaSlow'] = self.SMA(self.data_frame['close'], periodSlow)
 
-        for index, row in self.data_frame.iterrows():
-            if self.CrossAbove(row['smaFast'], row['smaSlow']):
-                self.EnterLong(index)
-            elif self.CrossBelow(row['smaFast'], row['smaSlow']):
-                self.EnterShort(index)
+        self.long_condition = self.CrossAbove(self.data_frame['smaFast'], self.data_frame['smaSlow'])
+        self.short_condition = self.CrossBelow(self.data_frame['smaFast'], self.data_frame['smaSlow'])
+
+        self.EnterLong()
+        self.EnterShort()
 
 # Example usage
 data = {'open': [105, 110, 100, 98, 105, 107, 103, 112, 118, 115],
